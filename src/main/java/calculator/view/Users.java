b82b31f2f3912c54ca69dffc0cc891bca03c0f5c
sytTest <syt1128@naver.com>
2024-10-15 18:11:59 +0900

    :sparkles:(Users) generate input method
    입력을 돕는 메시지 출력하고 입력받은 문자열을 반환하는 input 메서드 생성

---
+++ src/main/java/calculator/view/Users.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Users {
+
+    public static String input() {
+        System.out.println(Message.INPUT_GUIDE);
+        return readLine();
+    }
+}
